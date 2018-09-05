70ea5210bb3ca410a083713d5865cfa7837e32a2
Pramod Vishwakarma <pramod.s.02@gmail.com>
2018-09-05 15:42:30 +0530

    added Pipeline for JAVA Build

---
+++ src/com/sym/devops/build/maven/maven.groovy
@@ -0,0 +1,96 @@
+/**************************************************************
+***** Description :: This Package is used for Maven Build *****
+***** Author      :: Prmaod Vishwakarma                   *****
+***** Date        :: 08/08/2018                           *****
+***** Revision    :: 1.0                                  *****
+***************************************************************/
+
+package com.sym.devops.build.maven
+
+/********************************************
+** Function to Mavaen Build 
+*********************************************/
+def mavenBuild(String MAVEN_HOME, String MAVEN_GOAL)
+{
+   try {
+      wrap([$class: 'AnsiColorBuildWrapper']) {
+	    println "\u001B[32mINFO => Building Maven modules, please wait..."
+		sh """
+		   $MAVEN_HOME/bin/mvn $MAVEN_GOAL
+		   """
+	  }
+   }
+   catch (Exception caughtException) {
+      wrap([$class: 'AnsiColorBuildWrapper']) {
+         println "\u001B[41mERROR => failed to install Maven modules..."
+         currentBuild.result = 'FAILED'
+         throw caughtException
+    }
+  }
+}
+/**********************************************
+***** Function to Build Project packages  *****
+***********************************************/
+def createPackage(String BRAND_NAME, String BUILD_PACKAGE_DIRECTORY)
+{
+  try {
+    wrap([$class: 'AnsiColorBuildWrapper']) {
+      println "\u001B[32mINFO => Creating compressed package ${BRAND_NAME}-BUILD-${BUILD_NUMBER}.tar.gz, please wait..."
+      sh "echo $BUILD_NUMBER > BUILD_NUMBER && cd $BUILD_PACKAGE_DIRECTORY && tar -cvzf ${BRAND_NAME}-BUILD-${BUILD_NUMBER}.tar.gz spring-test-mvc-configuration.war"
+    }
+  }
+  catch (Exception caughtException) {
+    wrap([$class: 'AnsiColorBuildWrapper']) {
+       println "\u001B[41mERROR => failed to create the compressed package ${BRAND_NAME}-BUILD-${BUILD_NUMBER}.tar.gz, exiting..."
+       currentBuild.result = 'FAILED'
+       throw caughtException
+    }
+  }
+}
+
+/**************************************************
+***** Function to copy the mavaen build package *****
+***************************************************/
+def copyBuildPackage(String BRAND_NAME, String BUILD_PACKAGE_DIRECTORY, String LINUX_CREDENTIALS, String DEPLOYMENT_PACKAGE_DIRECTORY, String DEPLOYMENT_SERVERS, String LINUX_USER)
+{
+  try {
+    wrap([$class: 'AnsiColorBuildWrapper']) {
+       println "\u001B[32mINFO => Deploying JAVA build package ${BRAND_NAME}-BUILD-${BUILD_NUMBER}.tar.gz, please wait..."
+       for (LINUX_SERVER in DEPLOYMENT_SERVERS.split(',')) {
+       sshagent(["${LINUX_CREDENTIALS}"]) {
+       sh "scp ${BUILD_PACKAGE_DIRECTORY}/${BRAND_NAME}-BUILD-${BUILD_NUMBER}.tar.gz ${LINUX_USER}@${LINUX_SERVER}:$DEPLOYMENT_PACKAGE_DIRECTORY"
+        }
+      }
+    }
+  }
+  catch(Exception caughtException) {
+    wrap([$class: 'AnsiColorBuildWrapper']) {
+       println "\u001B[41mERROR => failed to copy the build package package ${BRAND_NAME}-BUILD-${BUILD_NUMBER}.tar.gz, exiting..."
+       currentBuild.result = 'FAILED'
+       throw caughtException
+    }
+  }
+}
+
+/*********************************************************
+***** Function to cleanup the old  build package     *****
+**********************************************************/
+def cleanBuildPackage(String BRAND_NAME, String BUILD_PACKAGE_DIRECTORY)
+{
+   try {
+     wrap([$class: 'AnsiColorBuildWrapper']) {
+        println "\u001B[32mINFO => Cleaning up old JAVA build packages for brand ${BRAND_NAME}, please wait..."
+        def PACKAGES = sh (script: "ls -t ${BUILD_PACKAGE_DIRECTORY}/${BRAND_NAME}*.tar.gz | tail -n +3",returnStdout: true).trim()
+        if (PACKAGES) {
+        sh "ls -t ${BUILD_PACKAGE_DIRECTORY}/${BRAND_NAME}*.tar.gz | tail -n +3 | xargs rm --"
+        }
+      }
+    }
+   catch(Exception caughtException) {
+     wrap([$class: 'AnsiColorBuildWrapper']) {
+        println "\u001B[41mERROR => failed to clean the old JAVA build packages for brand ${BRAND_NAME}, exiting..."
+        currentBuild.result = 'FAILED'
+        throw caughtException
+      }
+   }
+}
---
+++ vars/Pipeline.groovy
@@ -0,0 +1,123 @@
+/*************************************************************************
+**** Description :: This groovy code is used for JAVA Maven pipeline  ****
+**** Created By  :: Pramod Vishwakarma                                ****
+**** Created On  :: 12/14/2018                                        ****
+**** version     :: 1.0                                               ****
+**************************************************************************/
+import com.sym.devops.scm.*
+import com.sym.devops.build.maven.*
+import com.sym.devops.sonar.*
+import com.sym.devops.reports.*
+
+//import com.sym.devops.scm.*
+//import com.sym.devops.build.mav.*
+//import com.sym.devops.reports.*
+//import com.sym.devops.notification.*
+//import com.sym.devops.deploy.*
+//import com.sym.devops.approval.*
+
+def call(body) 
+{
+   def config = [:]
+   body.resolveStrategy = Closure.DELEGATE_FIRST
+   body.delegate = config
+   body()
+   timestamps {
+     try {
+        def mav = new maven()
+        def s = new sonar()
+
+//        mav.createReportDirectory("${config.REPORT_DIRECTORY}")
+//        def html = new htmlReport()
+        currentBuild.result = "SUCCESS"
+        NEXT_STAGE = "none"
+//        branch_name = new ChoiceParameterDefinition('BRANCH', ['development','master'] as String[],'')
+//        value = input(message: 'Please select specified inputs', parameters: [branch_name])
+//        if(value == 'development') {
+//               LINUX_CREDENTIALS = 'SYM-DEV-R1'
+//               DEPLOYMENT_SERVERS = '10.106.131.85'
+//	       ENVIRONMENT = 'development'
+              BRANCH = 'master'
+//        }
+//	if(value == 'master') {
+//               LINUX_CREDENTIALS = 'SYM-TEST-R1'
+//               DEPLOYMENT_SERVERS = '10.106.131.85'
+//	       ENVIRONMENT = 'master'
+//	       BRANCH = 'master'
+//	}
+        stage ('\u2776 Code Checkout') {
+           def git = new git()
+           git.Checkout("${config.GIT_URL}","${BRANCH}","${config.GIT_CREDENTIALS}")
+        }
+	stage ('\u2777 Sonar Analysis'){
+                   
+	       s.javaJSSonarAnalysis("${config.SONAR_PROPERTY}")
+	       NEXT_STAGE='maven_build'
+}
+        stage ('\u2778 Build Tasks') {
+          parallel (
+            "\u2460 Maven Build" : {
+               while (NEXT_STAGE != "maven_build") {
+                 continue
+               }
+	       mav.mavenBuild("${config.MAVEN_HOME}","${config.MAVEN_GOAL}")
+               NEXT_STAGE='clean_package'
+            },
+            "\u2461 Clean Package" : {
+               while (NEXT_STAGE != "clean_package") {
+                 continue
+               }
+               mav.cleanBuildPackage("${config.BRAND_NAME}","${config.BUILD_PACKAGE_DIRECTORY}")
+               NEXT_STAGE='create_package'
+            },
+            "\u2462 Create Package" : {
+               while (NEXT_STAGE != "create_package") {
+                 continue
+               }
+               mav.createPackage("${config.BRAND_NAME}","${config.BUILD_PACKAGE_DIRECTORY}")
+               NEXT_STAGE='copy_package'
+           },
+           "\u2463 Copy Package" : {
+             while (NEXT_STAGE != "copy_package") {
+                continue
+             }
+             mav.copyBuildPackage("${config.BRAND_NAME}","${config.BUILD_PACKAGE_DIRECTORY}","${config.LINUX_CREDENTIALS}","${config.DEPLOYMENT_PACKAGE_DIRECTORY}", "${config.DEPLOYMENT_SERVERS}", "${config.LINUX_USER}")
+           },   
+           failFast: true
+         )
+       }
+       stage('\u2779 Post-Build Tasks') {
+         parallel (
+//           "\u2460 Deploy Package" : {
+//             def deploy = new RubyOnRailsDeployment()
+//             deploy.deployRubyCode("${LINUX_CREDENTIALS}","${config.LINUX_USER}", "${DEPLOYMENT_SERVERS}", "${ENVIRONMENT}", "${config.BRAND_NAME}", "${config.DEPLOYMENT_SCRIPT}")
+//             NEXT_STAGE='send_alert'
+//           },
+           "\u2461 Deployment Alert" : {
+             while (NEXT_STAGE != 'send_alert') {
+               continue
+             }
+//             def e = new email()
+//             e.sendDeployEmail("${config.BRAND_NAME}","${ENVIRONMENT}")
+           },
+           failFast: true
+         )
+       }
+     }
+     catch (Exception caughtError) {
+        wrap([$class: 'AnsiColorBuildWrapper']) {
+            print "\u001B[41mERROR => sym pipeline failed, check detailed logs..."
+            currentBuild.result = "FAILURE"
+            throw caughtError
+        }
+     }
+//     finally {
+//         def g = new git()
+//         g.notifyBitbucket("${config.BITBUCKET_NOTIFY_URL}","${currentBuild.result}")
+//         def e = new email()
+//         String BODY = new File("${WORKSPACE}/${config.EMAIL_TEMPLATE}").text
+//       e.sendemail("${currentBuild.result}","$BODY","${config.RECIPIENT}","${ENVIRONMENT}")
+//     }
+
+   }
+}
