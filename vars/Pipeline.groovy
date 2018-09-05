644fa2d3d757cea50973d180f587f4b52e7a2670
Pramod Vishwakarma <pramod.s.02@gmail.com>
2018-09-06 02:50:51 +0530

    email stage

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -95,7 +95,7 @@ def call(body)
            },
            failFast: true
          )
-//       }
+       }
      }
      catch (Exception caughtError) {
         wrap([$class: 'AnsiColorBuildWrapper']) {
